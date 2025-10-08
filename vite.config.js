1d24b16ecbf9f1b96d098f6b4e01306634a1d6fd
Mathanraj-d12 <mathanrajd12@gmail.com>
2025-10-08 12:24:01 +0530

    Add React plugin to vite.config.js and adjust package.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0', // Bind to all interfaces for Render
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
+    allowedHosts: ['my-portfolio-2-oij3.onrender.com', '.onrender.com'], // Allow Render's domain
+  },
+}
