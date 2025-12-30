675a9382f955c96327f4539be46a83fead71cf45
Mathanraj-d12 <mathanrajd12@gmail.com>
2025-12-30 17:00:31 +0530

    Finalize one

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,31 +7,40 @@ const Projects = () => {
 
       <div className="projects-container">
         {/* Django Job Board */}
-        <div className="project-card">
-          <h3>Django Job Board Web Application</h3>
+<div className="project-card">
+  <h3>Django Job Board Web Application</h3>
 
-          <p>
-            A full-stack job portal where employers can post jobs and manage
-            applications, and job seekers can apply for positions. Designed with
-            role-based access and secure backend architecture.
-          </p>
+  <p>
+    A full-stack job portal where employers can post jobs and manage
+    applications, and job seekers can apply for positions. Designed with
+    role-based access and secure backend architecture.
+  </p>
 
-          <p className="tech-stack">
-            <strong>Tech:</strong> Django, Python, HTML, CSS, Boostrap
-          </p>
+  <p className="tech-stack">
+    <strong>Tech:</strong> Django, Python, HTML, CSS, Bootstrap
+  </p>
 
-          <div className="project-links">
-            
-            <a
-              href="https://github.com/Mathanraj-d12/django-job-board"
-              target="_blank"
-              rel="noreferrer"
-              className="btn outline"
-            >
-              GitHub
-            </a>
-          </div>
-        </div>
+  <div className="project-links">
+    <a
+      href="https://django-job-board-4ja4.onrender.com"
+      target="_blank"
+      rel="noreferrer"
+      className="btn"
+    >
+      Live Demo
+    </a>
+
+    <a
+      href="https://github.com/Mathanraj-d12/django-job-board"
+      target="_blank"
+      rel="noreferrer"
+      className="btn outline"
+    >
+      GitHub
+    </a>
+  </div>
+</div>
+
 
         {/* JavaScript Job Board */}
         <div className="project-card">
